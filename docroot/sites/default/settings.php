26c560fb70ad9fd4dd32ad3d9550478918d514fd
Acquia Cloud <support@acquia.com>
2016-06-28 11:34:53 +0000

    Importing site archive

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -606,3 +606,12 @@ if (isset($conf['memcache_servers'])) {
     $conf['lock_inc'] = './sites/all/modules/memcache/memcache-lock.inc';
 }
 
+
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/ausgovhack/ausgovhack-settings.inc');
+}
